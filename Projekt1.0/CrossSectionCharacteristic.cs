746e9d4bf1d9039e339ecba63e12de8f6a656386
Olga Pietnoczko <olga.pietn@gmail.com>
2019-01-26 15:46:31 +0100

    Cross Section Characteristic init

---
+++ Projekt1.0/CrossSectionCharacteristic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1._0
+{
+    class CrossSectionCharacteristic
+    {
+        public double b
+        {
+            get;
+            set;
+
+        }
+        public double h
+        {
+            get;
+            set;
+
+        }
+
+        public double a
+        {
+            get
+            {
+                return h * b;
+            }
+        }
+
+
+
+    }
+}
+
+
+ 
